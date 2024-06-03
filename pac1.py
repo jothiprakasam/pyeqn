5cf92eab4c39e1743c0e93154f8b42cd133df225
jothiprakasam <mrameshsai74@gmail.com>
2024-06-03 21:19:09 +0530

    main pac1 file added

---
+++ pac1.py
@@ -0,0 +1,9 @@
+import math
+def QUADRATIC_EQN(a,b,c):
+       try:
+         res1 =(-b+math.sqrt(b**2 - 2*a*c))/2*a
+         return res1
+       except ValueError:
+             print("There is some error")
+
+    
