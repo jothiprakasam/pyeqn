b3296fd3112b8f91c3345af03e157a26e99caa36
Jothi prakash <71811173+jothiprakasam@users.noreply.github.com>
2024-06-03 22:06:42 +0530

    comments added

--- pac1.py
+++ pac1.py
@@ -1,9 +1,12 @@
 import math
+#Fuction for Quadratic equation
 def QUADRATIC_EQN(a,b,c):
        try:
          res1 =(-b+math.sqrt(b**2 - 4*a*c))/2*a
          return res1
        except ValueError:
              print("There is some error")
+
+
 
     
