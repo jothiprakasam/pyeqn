d548b7cce5c4314e1f020d9b2532eb5952ff62c4
jothiprakasam <mrameshsai74@gmail.com>
2024-06-03 22:38:30 +0530

    return both values as tuple

--- pyeqn.py
+++ pyeqn.py
@@ -3,7 +3,8 @@ import math
 def QUADRATIC_EQN(a,b,c):
        try:
          res1 =(-b+math.sqrt(b**2 - 4*a*c))/2*a
-         return res1
+         res2 =(-b-math.sqrt(b**2 - 4*a*c))/2*a
+         return (res1,res2)
        except ValueError:
              print("The equation has complex root")
 
